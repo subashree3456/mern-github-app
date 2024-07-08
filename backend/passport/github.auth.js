4b6f15fa94f77062470aab3d7c61913b90364a7d
SUBASHREE SANKARANARAYANAN <subashree3003@gmail.com>
2024-07-08 10:25:01 +0530

    Callback url updated

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -24,7 +24,7 @@ passport.use(
         {
             clientID: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            callbackURL: "/api/auth/github/callback",
+            callbackURL: "https://mern-github-app-ir02.onrender.com/api/auth/github/callback",
         },
         async function (accessToken, refreshToken, profile, done) {
             const user = await User.findOne({ username: profile.username });
